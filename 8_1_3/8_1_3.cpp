311749822edd0a822473bb97793724a533f89bb5
Molibog Roman <90984207+liner-exe@users.noreply.github.com>
2025-12-24 21:12:59 +0300

    LZ78 done

---
+++ 8_1_3/8_1_3.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+
+void encodeL78(std::string text)
+{
+	std::map<std::string, int> dict;
+	int dictSize = 1;
+	std::string current = "";
+
+	std::cout << "Результат метода LZ78" << std::endl;
+
+	for (int i = 0; i < text.length(); i++)
+	{
+		current += text[i];
+		if (dict.find(current) == dict.end())
+		{
+			std::string prefix = current.substr(0, current.length() - 1);
+			int index = prefix == "" ? 0 : dict[prefix];
+
+			std::cout << "(" << index << ',' << text[i] << ')' << std::endl;
+
+			dict[current] = dictSize++;
+			current = "";
+		}
+	}
+
+	if (!current.empty())
+	{
+		std::string prefix = current.substr(0, current.length() - 1);
+		int index = prefix == "" ? 0 : dict[prefix];
+		std::cout << "(" << index << ',' << "EOF" << ')' << std::endl;
+	}
+}
+
+int main()
+{
+	std::string data = "roporopoterropoterter";
+	encodeL78(data);
+	return 0;
+}
